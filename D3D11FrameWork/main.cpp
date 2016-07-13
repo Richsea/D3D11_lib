6a7bf96a237d45967fe8f872ee821e31a24de34a
Richsea <201402377@cs-cnu.org>
2016-07-13 15:22:38 +0900

    UnSharp 작업
    1. RT.hlsl 파일을 추가시켜 줌
    2, RT를 적용시키기 위한 main 추가

--- D3D11FrameWork/main.cpp
+++ D3D11FrameWork/main.cpp
@@ -7,9 +7,15 @@ public:
 	ID3D11VertexShader * vertexShader;
 	ID3D11PixelShader * pixelShader;
 	ID3D11Texture2D * texture;
-	ID3D11ShaderResourceView * textureShaderResourceView;
+	ID3D11ShaderResourceView * textureSRV;
 
+	ID3D11RenderTargetView * renderTarget;
+	ID3D11Texture2D * renderTargetBuffer;
+	ID3D11ShaderResourceView * renderTargetBufferSRV;
 	ID3D11SamplerState * samplerState;
+	ID3D11VertexShader * renderTargetVertexShader;
+	ID3D11PixelShader * renderTargetPixelShader;
+	ID3D11InputLayout * renderTargetInputLayout;
 
 public:
 	void onInitialize()
@@ -40,6 +46,7 @@ public:
 		void * textureData;
 		unsigned textureWidth, textureHeight;
 		PRGetImageData(std::string("test.png"), &textureData, &textureWidth, &textureHeight);
+		
 		D3D11_TEXTURE2D_DESC textureDesc = { 0, };
 		textureDesc.ArraySize = 1;
 		textureDesc.Width = textureWidth;
@@ -50,12 +57,29 @@ public:
 		//textureDesc.CPUAccessFlags = D3D11_CPU_ACCESS_READ | D3D11_CPU_ACCESS_WRITE;
 		textureDesc.Usage = D3D11_USAGE_DEFAULT;
 		textureDesc.BindFlags = D3D11_BIND_SHADER_RESOURCE;
-		D3D11_SUBRESOURCE_DATA textureInput = { textureData, textureWidth * sizeof(int), textureWidth * textureHeight * sizeof(int) };
-		graphicsContext->d3dDevice->CreateTexture2D(&textureDesc, &textureInput, &texture);
-		graphicsContext->d3dDevice->CreateShaderResourceView(texture, nullptr, &textureShaderResourceView);
-
+		D3D11_SUBRESOURCE_DATA textureSubResource = { textureData, textureWidth * sizeof(int), textureWidth * textureHeight * sizeof(int) };
+		graphicsContext->d3dDevice->CreateTexture2D(&textureDesc, &textureSubResource, &texture);
+		
 		delete[] textureData;
+		
+		graphicsContext->d3dDevice->CreateShaderResourceView(texture, nullptr, &textureSRV);
 
+		D3D11_TEXTURE2D_DESC renderTargetDesc = { 0, };
+		renderTargetDesc.ArraySize = 1;
+		renderTargetDesc.Width = 1280;
+		renderTargetDesc.Height = 720;
+		renderTargetDesc.MipLevels = 1;
+		renderTargetDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+		renderTargetDesc.SampleDesc.Count = 1;
+		//renderTargetDesc.CPUAccessFlags = D3D11_CPU_ACCESS_READ | D3D11_CPU_ACCESS_WRITE;
+		renderTargetDesc.Usage = D3D11_USAGE_DEFAULT;
+		renderTargetDesc.BindFlags = D3D11_BIND_SHADER_RESOURCE | D3D11_BIND_RENDER_TARGET;
+		graphicsContext->d3dDevice->CreateTexture2D(&renderTargetDesc, nullptr, &renderTargetBuffer);
+
+		graphicsContext->d3dDevice->CreateRenderTargetView(renderTargetBuffer, nullptr, &renderTarget);
+
+		graphicsContext->d3dDevice->CreateShaderResourceView(renderTargetBuffer, nullptr, &renderTargetBufferSRV);
+		
 		/**
 		SamplerState는 원근법 설정(가까이 오면 커지고, 멀리가면 작아진다)
 		*/
@@ -70,12 +94,31 @@ public:
 		samplerDesc.ComparisonFunc = D3D11_COMPARISON_NEVER;
 		graphicsContext->d3dDevice->CreateSamplerState(&samplerDesc, &samplerState);
 
+		PRGetRawData(std::string("MyVertexShaderRT.cso"), &shaderData, &shaderDataLength);
+		graphicsContext->d3dDevice->CreateVertexShader(shaderData, shaderDataLength, nullptr, &renderTargetVertexShader);
+		D3D11_INPUT_ELEMENT_DESC inputElementsRT[] = {
+			{"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0},
+			{"TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+		};
+		graphicsContext->d3dDevice->CreateInputLayout(inputElementsRT, _countof(inputElementsRT), 
+			shaderData, shaderDataLength, &renderTargetInputLayout);
+		SAFE_DELETE(shaderData);
+		PRGetRawData(std::string("MyPixelShaderRT.cso"), &shaderData, &shaderDataLength);
+		graphicsContext->d3dDevice->CreatePixelShader(shaderData, shaderDataLength, nullptr, &renderTargetPixelShader);
+		SAFE_DELETE(shaderData);
+
 	}
 
 	void onDestory()
 	{
+		renderTargetInputLayout->Release();
+		renderTargetPixelShader->Release();
+		renderTargetVertexShader->Release();
 		samplerState->Release();
-		textureShaderResourceView->Release();
+		renderTargetBufferSRV->Release();
+		renderTarget->Release();
+		renderTargetBuffer->Release();
+		textureSRV->Release();
 		texture->Release();
 		pixelShader->Release();
 		vertexShader->Release();
@@ -87,13 +130,15 @@ public:
 	{
 		GETGRAPHICSCONTEXT(PRGraphicsContext_Direct3D11);
 
-		float clearColor[] = { 0, 0, 0, 1 };
-		graphicsContext->immediateContext->ClearRenderTargetView(graphicsContext->renderTargetView, clearColor);
-		graphicsContext->immediateContext->ClearDepthStencilView(graphicsContext->depthStencilView, D3D11_CLEAR_DEPTH, 1, 0);
+		float clearColor[] = { 1, 0, 1, 1 };
+		ID3D11ShaderResourceView * pSRV[] = { nullptr };
+		graphicsContext->immediateContext->PSSetShaderResources(0, 1, pSRV);
+		graphicsContext->immediateContext->OMSetRenderTargets(1, &renderTarget, nullptr);
+		graphicsContext->immediateContext->ClearRenderTargetView(renderTarget, clearColor);
 
 		graphicsContext->immediateContext->VSSetShader(vertexShader, nullptr, 0);
 		graphicsContext->immediateContext->PSSetShader(pixelShader, nullptr, 0);
-		graphicsContext->immediateContext->PSSetShaderResources(0, 1, &textureShaderResourceView);
+		graphicsContext->immediateContext->PSSetShaderResources(0, 1, &textureSRV);
 		graphicsContext->immediateContext->PSSetSamplers(0, 1, &samplerState);	//sample : 색상
 
 		unsigned stride = sizeof(PRVec3) + sizeof(PRVec2), offset = 0;
@@ -102,6 +147,22 @@ public:
 		graphicsContext->immediateContext->IASetInputLayout(inputLayout);
 
 		graphicsContext->immediateContext->Draw(6, 0);
+
+		float clearColor1[] = { 0, 0, 0, 1 };
+		graphicsContext->immediateContext->OMSetRenderTargets(1, &graphicsContext->renderTargetView, nullptr);
+		graphicsContext->immediateContext->ClearRenderTargetView(graphicsContext->renderTargetView, clearColor1);
+
+		graphicsContext->immediateContext->VSSetShader(renderTargetVertexShader, nullptr, 0);
+		graphicsContext->immediateContext->PSSetShader(renderTargetPixelShader, nullptr, 0);
+		graphicsContext->immediateContext->PSSetShaderResources(0, 1, &renderTargetBufferSRV);
+		graphicsContext->immediateContext->PSSetSamplers(0, 1, &samplerState);
+
+		graphicsContext->immediateContext->IASetVertexBuffers(0, 1, &vertexBuffer, &stride, &offset);
+		graphicsContext->immediateContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+		graphicsContext->immediateContext->IASetInputLayout(renderTargetInputLayout);
+
+		graphicsContext->immediateContext->Draw(6, 0);
+
 
 		graphicsContext->dxgiSwapChain->Present(0, 0);
 	}
